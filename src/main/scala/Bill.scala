abdaa89fbd091ece63b6f4b0c01b3fb1068201d4
paul.campbell <paul.campbell@digital.hmrc.gov.uk>
2017-04-25 11:26:44 -0700

    Step 2.1 - add a 20/10 % charge rate as applicable

--- src/main/scala/Bill.scala
+++ src/main/scala/Bill.scala
@@ -1,13 +1,41 @@
 
-sealed abstract class Item (description: String, val price: BigDecimal)
+object ServiceChargeValues {
+  val DefaultRate = 0
+  val FoodRate = 10
+  val HotFoodRate = 20
+  val ServiceChargeCap = 20
+}
+
+import ServiceChargeValues._
+
 // although description isn't used yet it provides if nothing else a cut and dried mapping to the stated problem domain
+sealed abstract class Item (description: String, val price: BigDecimal) {
+  def serviceChargeRate: Int = DefaultRate
+}
+
+sealed abstract class Food (description: String, price: BigDecimal, val isHot: Boolean) extends Item(description, price) {
+  override def serviceChargeRate: Int = if(isHot) HotFoodRate else FoodRate
+}
 
 case object Cola extends Item("Cola", BigDecimal("0.50"))
 case object Coffee extends Item("Coffee", BigDecimal("1.00"))
-case object CheeseSandwich extends Item("Cheese Sandwich", BigDecimal("2.00"))
-case object SteakSandwich extends Item("Steak Sandwich", BigDecimal("4.50"))
+case object CheeseSandwich extends Food("Cheese Sandwich", BigDecimal("2.00"), isHot = false)
+case object SteakSandwich extends Food("Steak Sandwich", BigDecimal("4.50"), isHot = true)
 
 case class Bill(items: Seq[Item]) {
-  def total : BigDecimal = items.map(_.price).fold(BigDecimal(0)){ (p1, p2) =>  p1 + p2}
+
+
+  def total: BigDecimal = grossTotal + calculatePercent(grossTotal, applicableServiceRate)
+
+  def grossTotal: BigDecimal = items.map(_.price).fold(BigDecimal(0)){(p1, p2) =>  p1 + p2}
+
+  private def applicableServiceRate: Int = {
+    items.map(_.serviceChargeRate) match {
+      case Seq() => DefaultRate
+      case rates => rates.max
+    }
+  }
+
+  private def calculatePercent(value: BigDecimal, percentage: Int): BigDecimal = value * (percentage / BigDecimal(100))
 }
 
--- src/test/scala/BillTest.scala
+++ src/test/scala/BillTest.scala
@@ -4,18 +4,34 @@ import org.scalatest.{FlatSpec, Matchers}
 class SetSpec extends FlatSpec with Matchers {
 
   "an empty bill" should "have no cost" in {
-    Bill(items = Seq.empty).total shouldBe BigDecimal("0")
+    Bill(items = Seq.empty).grossTotal shouldBe BigDecimal("0")
   }
 
   "single items" should "have correct single item cost" in {
-    Bill(items = Seq(Cola)).total shouldBe BigDecimal("0.50")
-    Bill(items = Seq(Coffee)).total shouldBe BigDecimal("1.00")
-    Bill(items = Seq(CheeseSandwich)).total shouldBe BigDecimal("2.00")
-    Bill(items = Seq(SteakSandwich)).total shouldBe BigDecimal("4.50")
+    Bill(items = Seq(Cola)).grossTotal shouldBe BigDecimal("0.50")
+    Bill(items = Seq(Coffee)).grossTotal shouldBe BigDecimal("1.00")
+    Bill(items = Seq(CheeseSandwich)).grossTotal shouldBe BigDecimal("2.00")
+    Bill(items = Seq(SteakSandwich)).grossTotal shouldBe BigDecimal("4.50")
   }
 
   "multiple items" should "have cost which is sum of item costs" in {
-    Bill(items = Seq(Cola, Coffee, CheeseSandwich)).total shouldBe BigDecimal("3.50")
-    Bill(items = Seq(Cola, Coffee, Coffee)).total shouldBe BigDecimal("2.50")
+    Bill(items = Seq(Cola, Coffee, CheeseSandwich)).grossTotal shouldBe BigDecimal("3.50")
+    Bill(items = Seq(Cola, Coffee, Coffee)).grossTotal shouldBe BigDecimal("2.50")
+  }
+
+  "an empty bill" should "should have no cost including service charge" in {
+    Bill(items = Seq.empty).total shouldBe BigDecimal("0")
+  }
+
+  "all drinks bill" should "not incur service charge" in {
+    Bill(items = Seq(Cola, Coffee)).total shouldBe BigDecimal("1.50")
+  }
+
+  "cold food single item" should "should incur service charge of 10%" in {
+    Bill(items = Seq(CheeseSandwich)).total shouldBe BigDecimal("2.20")
+  }
+
+  "hot food single item" should "should incur service charge of 20%" in {
+    Bill(items = Seq(SteakSandwich)).total shouldBe BigDecimal("5.40")
   }
 }
